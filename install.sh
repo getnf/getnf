6dfa01a110084c9aba3b971a5e9822c878931c10
Arthur McLain <mclain.it@gmail.com>
2023-11-27 18:52:04 +0100

    feat: update install.sh (#26)
    * feat: update install.sh
    
    * fix: remove old getnf before remote install

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-OUTDIR="$HOME/.local/bin/"
-[ -e "$OUTDIR" ] || mkdir -p $OUTDIR
-state1=$(ls -1 "$OUTDIR" | grep -E "getnf")
-CurrentDIR=$(pwd)
-PCurrentDir=$(echo "$CurrentDIR")
+DEST="$HOME/.local/bin"
+GETNFLOC="$DEST/getnf"
 
-[ -n "$state1" ] && rm "$OUTDIR"getnf
+# -p will not error if the directory already exists
+mkdir -p "$DEST"
 
-cp "$PCurrentDir"/getnf "$OUTDIR"
+if [ -n "$CURL_COMMAND" ]; then
+    #  if the script was invoked by curl
+    rm -f "$GETNFLOC"
 
+    # -f: fail fast with no output at all on server errors
+    # -s: be silent
+    # -S: but show an error message if it fails
+    # -L: follow redirects
+    # -O: write output to a local file named like the remote file
+    curl -fsSLO https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
+else
+    cp --remove-destination "$(pwd)/getnf" "$GETNFLOC"
+fi
