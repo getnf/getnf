547f6aad381655340090608c7bb9c1b18257a961
Arthur McLain <mclain.it@gmail.com>
2023-11-28 01:27:28 +0100

    fix(install): switch to using `curl`

--- install.sh
+++ install.sh
@@ -6,16 +6,11 @@ GETNFLOC="$DEST/getnf"
 # -p will not error if the directory already exists
 mkdir -p "$DEST"
 
-if [ -n "$CURL_COMMAND" ]; then
-    #  if the script was invoked by curl
-    rm -f "$GETNFLOC"
+rm -f "$GETNFLOC"
 
-    # -f: fail fast with no output at all on server errors
-    # -s: be silent
-    # -S: but show an error message if it fails
-    # -L: follow redirects
-    # -O: write output to a local file named like the remote file
-    curl -fsSLO https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
-else
-    cp --remove-destination "$(pwd)/getnf" "$GETNFLOC"
-fi
+# -f: fail fast with no output at all on server errors
+# -s: be silent
+# -S: but show an error message if it fails
+# -L: follow redirects
+# -O: write output to a local file named like the remote file
+curl -fsSLO https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
