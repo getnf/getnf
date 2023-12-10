43ba82ccbb2aa1297c8bf2ec39c487c887568609
Arthur McLain <mclain.it@gmail.com>
2023-12-10 11:01:43 +0100

    feat: add a flag to suppress output during install
    The flag is `-s` or `--silent`

--- install.sh
+++ install.sh
@@ -7,11 +7,20 @@ GREEN=$(tput setaf 2)
 BLUE=$(tput setaf 4)
 RESET=$(tput sgr0)
 
-echo "${BLUE}Installing getnf...${RESET}"
+# add the -s or --silent flag to suppress output
+SILENT=$([[ "$1" == "-s" ]] || [[ "$1" == "--silent" ]] && echo true || echo false)
+
+$SILENT || echo "${BLUE}Installing getnf...${RESET}"
 
 mkdir -p "$DEST"
 rm -f "$GETNFLOC"
-curl -fLO# https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
+
+if $SILENT; then
+    curl -fsSLO https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
+else
+    curl -fLO# https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output-dir "$DEST"
+fi
+
 chmod 755 "$GETNFLOC"
 
-echo "${GREEN}Installation finished${RESET}"
+$SILENT || echo "${GREEN}Installation finished${RESET}"
