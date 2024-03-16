13d749d174e69baa83aee4446a679b043e761ba8
Arthur McLain <mclain.it@gmail.com>
2024-03-16 03:42:40 +0100

    feat: update source url
    Update the branch and the repo in the url after switching to the org

--- install.sh
+++ install.sh
@@ -4,21 +4,20 @@ DEST="$HOME/.local/bin"
 GETNFLOC="$DEST/getnf"
 
 GREEN=$(tput setaf 2)
-BLUE=$(tput setaf 4)
 RESET=$(tput sgr0)
 
 # add the -s or --silent flag to suppress output
 SILENT=$([[ "$1" == "-s" ]] || [[ "$1" == "--silent" ]] && echo true || echo false)
 
-$SILENT || echo "${BLUE}Installing getnf...${RESET}"
+$SILENT || echo "Installing getnf..."
 
 mkdir -p "$DEST"
 rm -f "$GETNFLOC"
 
 if $SILENT; then
-    curl -fsSL# https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output getnftemp
+    curl -fsSL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnftemp
 else
-    curl -fL# https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf --output getnftemp
+    curl -fL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnftemp
 fi
 
 mv getnftemp "$GETNFLOC"
