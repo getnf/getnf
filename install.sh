f53bbbd6a9a6528ec23c27748233e03e61cb81ee
Arthur McLain <mclain.it@gmail.com>
2024-03-27 10:11:27 +0100

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -3,24 +3,28 @@
 DEST="$HOME/.local/bin"
 GETNFLOC="$DEST/getnf"
 
-GREEN=$(tput setaf 2)
-RESET=$(tput sgr0)
-
-# add the -s or --silent flag to suppress output
-SILENT=$([[ "$1" == "-s" ]] || [[ "$1" == "--silent" ]] && echo true || echo false)
-
-$SILENT || echo "Installing getnf..."
-
-mkdir -p "$DEST"
-rm -f "$GETNFLOC"
-
-if $SILENT; then
-    curl -fsSL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnftemp
-else
-    curl -fL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnftemp
+if command -v tput > /dev/null; then
+    GREEN=$(tput setaf 2)
+    RESET=$(tput sgr0)
 fi
 
-mv getnftemp "$GETNFLOC"
+# add the -s or --silent flag to suppress output
+SILENT='false'
+if [[ "$1" == "-s" || "$1" == "--silent" ]]; then
+    SILENT='true'
+fi
+
+[[ "$SILENT" == "true" ]] || echo "Installing getnf..."
+
+mkdir -p "$DEST"
+
+if [[ "$SILENT" == "true" ]]; then
+    curl -fsSL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnf.tmp
+else
+    curl -fL# https://raw.githubusercontent.com/getnf/getnf/main/getnf --output getnf.tmp
+fi
+
+mv -f getnf.tmp "$GETNFLOC"
 chmod 755 "$GETNFLOC"
 
-$SILENT || echo "${GREEN}Installation finished${RESET}"
+[[ "$SILENT" == "true" ]] || echo "${GREEN}Installation finished${RESET}"
